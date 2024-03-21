a13e2dff06511f3e0e28695b98a571217b975b73
[Jerry Chow] <142858979+JerryChowMX@users.noreply.github.com>
2024-03-21 10:35:13 -0600

    added Routes configuration and npm i react-router-dom'

--- my-portfolio/src/main.jsx
+++ my-portfolio/src/main.jsx
@@ -1,10 +1,32 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+import AboutMe from './sections/AboutMe/AboutMe';
+import Contact from './sections/Contact/Contact';
+import Portfolio from './sections/Portfolio/Portfolio';
+import Resume from './sections/Resume/Resume';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+
+const Main = () => {
+  return (
+    <Router>
+      <Header />
+      <main>
+        
+        {/* Scroll Restoration or further configurations can be added here */}
+        <Routes>
+          <Route path="/" element={<AboutMe />} />
+          <Route path="/portfolio" element={<Portfolio />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/resume" element={<Resume />} />
+
+          {/* Here I would add more routes as needed */}
+        </Routes>
+
+      </main>
+      <Footer />
+    </Router>
+  );
+};
+
+export default Main;
